b745042a6820fe7d22e76493742a3a18bee7adce
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-06-19 16:17:11 -0400

    Add Homepage title

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,3 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import withRoot from '../theme/withRoot';
+import IndexPage from '../components/IndexPage';
 
-const style = {
-  root: {
-    padding: '3rem',
-  },
-  title: {
-    flex: 1,
-  },
-};
-
-function IndexPage({ classes }) {
-  return (
-    <main className={classes.root}>
-      <Typography variant="h1" className={classes.title}>
-        Gatsby MUI Boilerplate
-      </Typography>
-      <Typography variant="subtitle1">
-        with jest and storybook, and JSS
-      </Typography>
-    </main>
-  );
-}
-
-export default withRoot(withStyles(style)(IndexPage));
+export default IndexPage;
