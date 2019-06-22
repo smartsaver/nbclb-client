19032a64eda1c557eac3943ffbd527b0ff0a6b3a
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-06-22 15:18:24 -0400

    Add Padding

--- src/components/Section.js
+++ src/components/Section.js
@@ -5,6 +5,10 @@ import withStyles from '@material-ui/core/styles/withStyles';
 const styles = theme => ({
   root: {
     minHeight: '10rem',
+    padding: '3rem 1rem',
+    [theme.breakpoints.up('md')]: {
+      padding: '3rem 4rem',
+    },
   },
   container: {
     margin: '0 auto',
@@ -24,7 +28,7 @@ function Section(props) {
   const { classes } = props;
   return (
     <section id={props.name} className={`${classes.root} ${props.className}`}>
-      <Typography variant="body1" component="div">
+      <Typography variant="body1" component="div" className={classes.container}>
         {props.children}
       </Typography>
     </section>
