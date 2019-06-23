8d9c253c3d419d202e69770a77c70692ac54d2b4
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-06-22 22:39:52 -0400

    Add open link in new tab in marked

--- src/libs/marked.js
+++ src/libs/marked.js
@@ -1,7 +1,15 @@
 import marked from 'marked';
 
+// open links in new tab
+var renderer = new marked.Renderer();
+renderer.link = function() {
+  var link = marked.Renderer.prototype.link.apply(this, arguments);
+  return link.replace('<a', "<a target='_blank' rel='noreferrer noopener'");
+};
+
 const defaultOption = {
   gfm: true,
+  renderer,
 };
 
 function markedWithDefault(str, option = defaultOption) {
