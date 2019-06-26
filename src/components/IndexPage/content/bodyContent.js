de51c6d1c1aa10f63b322257cab251b46b78c3c9
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-06-26 12:53:33 -0400

    Update Content

--- src/components/IndexPage/content/bodyContent.js
+++ src/components/IndexPage/content/bodyContent.js
@@ -28,15 +28,15 @@ const en = {
     marked(`
 Youth with savings for their education are 31% more likely to have a post-secondary mindset than those who have none.
 
-Children with education savings of up to $500 are 3 times more likely to enrol in post-secondary and 4 times more likely to graduate.
+Children with education savings of up to $500 are 3 times more likely to enroll in post-secondary and 4 times more likely to graduate.
 
-The Economic and Social Inclusion Corporation is working to increase post-secondary opportunities to youth. By focusing on the 47,000 + children who are eligible for the Canada Learning Bond, we can build a better future for our children and province.
+The Economic and Social Inclusion Corporation is working to increase post-secondary opportunities for youth. By focusing on the 47,000 + children who are eligible for the Canada Learning Bond, we can build a better future for our children and province.
   `)
   ),
   funding: addContent(
     'What funding is available?',
     marked(`
-Eligible children receive $500 initially and $100 more each year they continue to be eligible. CLB is available up to the age of 15 or maximum $2,000.
+Eligible children receive $500 initially and $100 more each year they continue to be eligible. The CLB is available up to the age of 15 or a maximum of $2,000.
 
 You do not have to put any of your own money into the RESP to receive the Canada Learning Bond.
 
@@ -50,14 +50,14 @@ Families eligible for the Canada Learning Bond have:
 * Children born on or after January 1, 2004.
 * A net family income of $46,605 or less for a family with up to 3 kids.
 
-*For families with more than 3 children click here.
+*For families with more than 3 children [click here.](https://www.canada.ca/en/employment-social-development/services/learning-bond/eligibility.html )
   `)
   ),
   apply: addContent(
     'What do I need to apply?',
     marked(`
-Social Insurance Number (SIN) for you and your child.
-Have filed your latest income tax and benefit return.
+A Social Insurance Number for you and your child. You must have filed your latest income tax and benefit return.
+
 You can get help at a free tax preparation clinic near you and some clinics help with taxes all year.
 For more information, visit the nearest [Community Volunteer Income Tax Program clinic](https://apps.cra-arc.gc.ca/ebci/oecv/external/prot/cli_srch_01_ld.action) in your community.
   `)
