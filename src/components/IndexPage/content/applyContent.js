37e9d058b9caadc1f14f688859ffea0a4f18abd7
jcunanan05 <cunanan.jonathan1995@gmail.com>
2019-07-02 11:24:21 -0400

    Update French Content

--- src/components/IndexPage/content/applyContent.js
+++ src/components/IndexPage/content/applyContent.js
@@ -1,44 +1,54 @@
 import marked from '../../../libs/marked';
 import defaultMarked from 'marked';
 
-function addContent(title, text, footer) {
+function addContent(title, text, footer, respUrl) {
   return {
-    ...neutralContent,
     title,
     text,
     footer,
+    respUrl,
   };
 }
 
-const neutralContent = {
-  respUrl: 'https://mysmartfuture.org/clbnb',
+const respUrl = {
+  en: 'https://mysmartfuture.org/clbnb',
+  fr: 'https://moneducavenir.org/clbnb',
 };
 
 const applyContent = {
   en: addContent(
     'Apply For Your Child’s Canada Learning Bond Today!',
     marked(`
-If you cannot see the form, [click here.](${neutralContent.respUrl})
+If you cannot see the form, [click here.](${respUrl.en})
   `),
     marked(`
 For more information contact us at 1-855-737-7252 (1-855-RESP-CLB) or info@smartsaver.org.
-  `)
+  `),
+    respUrl.en
   ),
   fr: addContent(
     'Demandez le Bon d’études canadien pour votre enfant dès aujourd’hui !',
     marked(`
-Si votre enfant est né en 2004 ou après et que votre revenu familial net est de 46 605 $* ou moins, vous pouvez profiter de cette occasion d’épargner dès aujourd’hui pour les études de votre enfant. Vous n’avez aucuns frais à payer pour demander le Bon d’études canadien ni aucun argent à investir.
+Si votre enfant est né en 2004 ou après et que votre revenu familial net est de 46 605 $* ou moins, vous pouvez profiter de cette occasion d’épargner dès aujourd’hui pour les études de votre enfant. Vous n’avez aucun frais à payer pour demander le Bon d’études canadien, ni aucune cotisation à faire.
 
 *pour les familles de trois enfants ou moins. Si votre famille compte plus de trois enfants, [cliquez ici](https://www.canada.ca/fr/emploi-developpement-social/services/aide-financiere-etudiants/epargne-etudes/reee/promoteurs-reee/bulletin/avis-2018-772.html).`) +
       defaultMarked(`
 Les enfants admissibles reçoivent un premier dépôt de 500 $ et 100 $ supplémentaires chaque année où ils sont admissibles, y compris les années précédant leur demande, jusqu’à un maximum de 2000 $.
 Pour recevoir le Bon d’études canadien, votre enfant doit avoir souscrit à un Régime enregistré d’épargne-études (REEE). Vous pouvez en obtenir un sans frais en [cliquant ici](./#resp).`) +
-      marked(
-        `Si vous ne pouvez pas voir le formulaire, [cliquez ici](${neutralContent.respUrl}). `
-      ),
+      marked(`Le lien ne s’ouvre pas, [cliquez ici](${respUrl.fr}). `),
     marked(`
-Pour plus d'information contactez 1-855-737-7252 ou info@smartsaver.org
-  `)
+Pour en savoir plus, composez le 1-855-737-7252 ou contactez info@smartsaver.org.
+
+Ou communiquez avec 1 800 O-Canada:
+
+__Téléphone :__ 1 800 O-Canada (1-800-622-6232)
+
+__ATS :__ 1-800-926-9105
+
+__À l’extérieur du Canada :__ Composer le 1 800 O-Canada à l'étranger
+
+  `),
+    respUrl.fr
   ),
 };
 
--- src/components/IndexPage/content/bodyContent.js
+++ src/components/IndexPage/content/bodyContent.js
@@ -16,7 +16,7 @@ const fr = {
   didYouKnow: addContent(
     '',
     marked(`
-Vérifiez si votre enfant est admissible au Bon d’études canadien ! Cette bourse du gouvernement du Canada, d’un montant allant de 500 $ à 2000 $, est déposée directement dans le Régime enregistré d’épargne-études (REEE) des enfants admissibles et est utilisable pour défrayer les coûts liés aux études postsecondaires.
+Vérifiez si votre enfant est admissible au Bon d’études canadien ! Cet incitatif à l’épargne-études du gouvernement du Canada, d’un montant allant de 500 $ à 2000 $, est déposée directement dans le Régime enregistré d’épargne-études (REEE) des enfants admissibles et est utilisable pour défrayer les coûts liés aux études postsecondaires.
   `)
   ),
 };
